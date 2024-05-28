a28e224a919e935a7c3c6f49dce902b2ad693b59
Frikh-Said <frikh.said@hotmail.com>
2024-05-28 15:27:25 +0100

    Front-end

---
+++ Front-end/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+
+export const routes: Routes = [
+    {
+        path:"",component:ChatbotComponent
+    }
+];
+
